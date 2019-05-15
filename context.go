8de8e8df09b0e40aad7f529c4d158b89d6676291
Vallimamod Abdullah <vma@sip.solutions>
2019-05-15 11:02:56 +0200

    adds GetBulkWithCtx and GetNexWithCtx api

--- context.go
+++ context.go
@@ -41,20 +41,36 @@ func (x *GoSNMP) GetWithCtx(ctx context.Context, oids []string) (result *SnmpPac
 	}
 }
 
-// BulkWalkWithCtx calls BulkWalk with a context
-func (x *GoSNMP) BulkWalkWithCtx(ctx context.Context, rootOid string, walkFn WalkFunc) error {
-	errCh := make(chan error)
+// GetNextWithCtx calls GetNext with a context
+func (x *GoSNMP) GetNextWithCtx(ctx context.Context, oids []string) (result *SnmpPacket, err error) {
+	snmpRes := make(chan snmpResult)
 	go func() {
-		errCh <- x.BulkWalk(rootOid, walkFn)
+		pkt, err := x.GetNext(oids)
+		snmpRes <- snmpResult{pkt, err}
 	}()
 	select {
 	case <-ctx.Done():
 		x.Conn.Close()
-		return fmt.Errorf("snmp bulk walk cancelled")
-	case err := <-errCh:
-		return err
+		return nil, fmt.Errorf("snmp get next cancelled")
+	case res := <-snmpRes:
+		return res.pkt, res.err
 	}
-	return nil
+}
+
+// GetBulkWithCtx calls GetBulk with a context
+func (x *GoSNMP) GetBulkWithCtx(ctx context.Context, oids []string, nonRepeaters uint8, maxRepetitions uint8) (result *SnmpPacket, err error) {
+	snmpRes := make(chan snmpResult)
+	go func() {
+		pkt, err := x.GetBulk(oids, nonRepeaters, maxRepetitions)
+		snmpRes <- snmpResult{pkt, err}
+	}()
+	select {
+	case <-ctx.Done():
+		x.Conn.Close()
+		return nil, fmt.Errorf("snmp get bulk cancelled")
+	case res := <-snmpRes:
+		return res.pkt, res.err
+	}
 }
 
 // WalkWithCtx calls Walk with a context
@@ -67,6 +83,22 @@ func (x *GoSNMP) WalkWithCtx(ctx context.Context, rootOid string, walkFn WalkFun
 	case <-ctx.Done():
 		x.Conn.Close()
 		return fmt.Errorf("snmp walk cancelled")
+	case err := <-errCh:
+		return err
+	}
+	return nil
+}
+
+// BulkWalkWithCtx calls BulkWalk with a context
+func (x *GoSNMP) BulkWalkWithCtx(ctx context.Context, rootOid string, walkFn WalkFunc) error {
+	errCh := make(chan error)
+	go func() {
+		errCh <- x.BulkWalk(rootOid, walkFn)
+	}()
+	select {
+	case <-ctx.Done():
+		x.Conn.Close()
+		return fmt.Errorf("snmp bulk walk cancelled")
 	case err := <-errCh:
 		return err
 	}
